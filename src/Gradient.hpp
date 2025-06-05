a4e1faea2c4f9dce31ef724f4e7baa354229260d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-05 22:35:39 +0200

    refactor

--- src/Gradient.hpp
+++ src/Gradient.hpp
@@ -5,10 +5,20 @@
 
 #include "Rgb.hpp"
 
+static u8 u8FromFloat(float x) {
+    if (x > 255.0f) {
+        return 255;
+    } else if (x < 0.0f) {
+        return 0;
+    } else {
+        return static_cast<u8>(std::trunc(x));
+    }
+}
+
 static u8 lerpU8(u8 start, u8 end, float x) {
     const float asFloat =
         static_cast<float>(start) * (1.0f - x) + static_cast<float>(end) * x;
-    return trunc(asFloat) > 255.0f ? 255 : trunc(asFloat);
+    return u8FromFloat(asFloat);
 }
 
 static Rgb lerpRgb(Rgb start, Rgb end, float x) {
@@ -22,10 +32,12 @@ static Rgb lerpRgb(Rgb start, Rgb end, float x) {
 class Gradient {
    public:
     [[nodiscard]] static Rgb get(float x) {
-        const float index = x / (colors.size() - 1);
-        const Rgb low = colors[trunc(index)];
-        const Rgb high = colors[trunc(index) + 1];
-        return lerpRgb(low, high, index - trunc(index));
+        const float floatIndex = x / (colors.size() - 1);
+        const usize index = static_cast<usize>(std::trunc(floatIndex));
+        const float offset = floatIndex - index;
+        const Rgb low = colors[index];
+        const Rgb high = colors[index + 1];
+        return lerpRgb(low, high, offset);
     }
 
    private:
