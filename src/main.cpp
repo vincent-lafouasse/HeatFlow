d49c7ad10a9dc5c7cabb3330c27ae768a86e7b0d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-06 12:49:42 +0200

    format

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@
 static constexpr int targetFps = 60;
 
 static constexpr usize gridMagnitude = 6;
-static constexpr usize gridSize = (1 << gridMagnitude); // 64px per big cell
-static constexpr usize gridSubdivision = 4; // cut tiles in 16
-static constexpr usize gridStep = gridSize / gridSubdivision; // 16px per sub-cell
+static constexpr usize gridSize = (1 << gridMagnitude);  // 64px per big cell
+static constexpr usize gridSubdivision = 4;              // cut tiles in 16
+static constexpr usize gridStep =
+    gridSize / gridSubdivision;  // 16px per sub-cell
 
 static constexpr usize bigGridWidth = 15;
 static constexpr usize bigGridHeight = 10;
@@ -29,10 +30,13 @@ int main() {
         ClearBackground(catpuccin::DarkGray.opaque());
 
         for (usize col = 0; col < gridWidth; ++col) {
-            const float horizontalAdvancement = static_cast<float>(col) / gridWidth * gridSubdivision;
+            const float horizontalAdvancement =
+                static_cast<float>(col) / gridWidth * gridSubdivision;
 
             for (usize row = 0; row < gridHeight; ++row) {
-                DrawRectangle(col * gridStep, row * gridStep, gridStep, gridStep, cmap.get(horizontalAdvancement).opaque());
+                DrawRectangle(col * gridStep, row * gridStep, gridStep,
+                              gridStep,
+                              cmap.get(horizontalAdvancement).opaque());
             }
         }
 
