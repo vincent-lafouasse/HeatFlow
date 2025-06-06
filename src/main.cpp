2f652183ccb85842ab574996340def8b993ac879
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-06 13:34:55 +0200

    sub grid

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,30 @@ static constexpr usize gridSize = 64;
 static constexpr usize gridWidth = 15;
 static constexpr usize gridHeight = 10;
 
-static constexpr int width = gridWidth * gridSize;
-static constexpr int height = gridHeight * gridSize;
+static constexpr int screenWidth = gridWidth * gridSize;
+static constexpr int screenHeight = gridHeight * gridSize;
+
+static constexpr usize gridSubdivision = 4;
+static constexpr usize gridStep = gridSize / gridSubdivision;
+static constexpr usize width = gridWidth * gridSubdivision;
+static constexpr usize height = gridHeight * gridSubdivision;
 
 int main() {
-    InitWindow(width, height, "hi");
+    InitWindow(screenWidth, screenHeight, "hi");
     SetTargetFPS(targetFps);
 
-    const ColorMap cmap = ColorMap::Inferno();
+    const ColorMap cmap = ColorMap::Viridis();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(catpuccin::DarkGray.opaque());
 
-        for (usize col = 0; col < gridWidth; ++col) {
-            for (usize row = 0; row < gridHeight; ++row) {
-                const float x = static_cast<float>(col + row) / (gridHeight + gridWidth);
-                DrawRectangle(col * gridSize, row * gridSize, gridSize,
-                              gridSize, cmap.get(x).opaque());
+        for (usize col = 0; col < width; ++col) {
+            for (usize row = 0; row < height; ++row) {
+                const float x =
+                    static_cast<float>(col + row) / (height + width);
+                DrawRectangle(col * gridStep, row * gridStep, gridStep,
+                              gridStep, cmap.get(x).opaque());
             }
         }
 
