f2b5bf6e5651102bc4925b720051104a42710268
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-06-06 13:27:53 +0200

    big grid

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,12 @@
 
 static constexpr int targetFps = 60;
 
-static constexpr usize gridMagnitude = 6;
-static constexpr usize gridSize = (1 << gridMagnitude);  // 64px per big cell
-static constexpr usize gridSubdivision = 4;              // cut tiles in 16
-static constexpr usize gridStep =
-    gridSize / gridSubdivision;  // 16px per sub-cell
+static constexpr usize gridSize = 64;
+static constexpr usize gridWidth = 15;
+static constexpr usize gridHeight = 10;
 
-static constexpr usize bigGridWidth = 15;
-static constexpr usize bigGridHeight = 10;
-static constexpr usize gridWidth = bigGridWidth * gridSubdivision;
-static constexpr usize gridHeight = bigGridHeight * gridSubdivision;
-
-static constexpr int width = gridWidth * gridStep;
-static constexpr int height = gridHeight * gridStep;
+static constexpr int width = gridWidth * gridSize;
+static constexpr int height = gridHeight * gridSize;
 
 int main() {
     InitWindow(width, height, "hi");
@@ -31,12 +24,13 @@ int main() {
 
         for (usize col = 0; col < gridWidth; ++col) {
             const float horizontalAdvancement =
-                static_cast<float>(col) / gridWidth * gridSubdivision;
+                static_cast<float>(col) / gridWidth;
 
             for (usize row = 0; row < gridHeight; ++row) {
-                DrawRectangle(col * gridStep, row * gridStep, gridStep,
-                              gridStep,
-                              cmap.get(horizontalAdvancement).opaque());
+                const float x = (row % 2) ? horizontalAdvancement
+                                          : 1.0f - horizontalAdvancement;
+                DrawRectangle(col * gridSize, row * gridSize, gridSize,
+                              gridSize, cmap.get(x).opaque());
             }
         }
 
